e4f2d772e98958e99271a541c1d487d3e4c72263
Micke Carlsson <mickecamino@gmail.com>
2008-09-09 10:48:20 +0000

    Addedd swedish translation for callwaiting

--- install.php
+++ install.php
@@ -1,15 +1,17 @@
 <?php
 
 // Register FeatureCode - Activate
+$cwa = _("Call Waiting - Activate");
 $fcc = new featurecode('callwaiting', 'cwon');
-$fcc->setDescription('Call Waiting - Activate');
+$fcc->setDescription($cwa);
 $fcc->setDefault('*70');
 $fcc->update();
 unset($fcc);
 
 // Register FeatureCode - Deactivate
+$cwd = _("Call Waiting - Deactivate")
 $fcc = new featurecode('callwaiting', 'cwoff');
-$fcc->setDescription('Call Waiting - Deactivate');
+$fcc->setDescription($cwd);
 $fcc->setDefault('*71');
 $fcc->update();
 unset($fcc);	
