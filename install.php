fd797646be00f8c1167c4ae9e501ee42eb9d85f8
Micke Carlsson <mickecamino@gmail.com>
2008-09-29 20:37:55 +0000

    Reverting previous changes, add strings for localization for callwaiting

--- install.php
+++ install.php
@@ -1,19 +1,22 @@
 <?php
+//for translation only
+if (false) {
+_("Call Waiting");
+_("Call Waiting - Activate");
+_("Call Waiting - Deactivate");
+}
 
 // Register FeatureCode - Activate
-$cwa = _("Call Waiting - Activate");
 $fcc = new featurecode('callwaiting', 'cwon');
-$fcc->setDescription($cwa);
+$fcc->setDescription('Call Waiting - Activate');
 $fcc->setDefault('*70');
 $fcc->update();
 unset($fcc);
 
 // Register FeatureCode - Deactivate
-$cwd = _("Call Waiting - Deactivate");
 $fcc = new featurecode('callwaiting', 'cwoff');
-$fcc->setDescription($cwd);
+$fcc->setDescription('Call Waiting - Deactivate');
 $fcc->setDefault('*71');
 $fcc->update();
 unset($fcc);	
-
 ?>
