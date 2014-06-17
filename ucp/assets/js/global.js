ff65734ed9e33eef647cdde2714807192bcb8ae8
Andrew Nagy <andrew.nagy@the159.com>
2014-06-17 15:14:53 -0400

    final changes for ucp

--- ucp/assets/js/global.js
+++ ucp/assets/js/global.js
@@ -1,15 +1,15 @@
-var CallwaitingC = UCPC.extend({
+var CallwaitingC = UCPMC.extend({
 	init: function(){
 	},
 	settingsDisplay: function() {
 		$('#cwenable').change(function() {
 			$.post( "index.php?quietmode=1&module=callwaiting&command=enable", {enable: $(this).is(':checked'), ext: ext}, function( data ) {
 				$('#module-Callwaiting .message').text(data.message).addClass('alert-'+data.alert).fadeIn('fast', function() {
-					$('.masonry-container').packery();
 					$(this).delay(2000).fadeOut('fast', function() {
 						$('.masonry-container').packery();
 					});
 				});
+				$('.masonry-container').packery();
 			});
 		});
 	},
