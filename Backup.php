df159fff5736c2af382a95f7ba8a3b099121c394
James Finstrom <jfinstrom@sangoma.com>
2018-04-27 02:32:42 +0000

    Fix method call in backup

--- Backup.php
+++ Backup.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Callwaiting;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
-    $configs = $this->FreePBX->bulkhandlerExport('extensions');
+    $configs = $this->FreePBX->Callwaiting->bulkhandlerExport('extensions');
     $this->addConfigs($configs);
   }
 }
