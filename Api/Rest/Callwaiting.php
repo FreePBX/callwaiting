7ab2193965a317736ffb07d29792dbadc2893002
Andrew Nagy <andrew.nagy@the159.com>
2018-09-05 14:20:18 -0400

    [Module Tag script: callwaiting 15.0.3] Rest Api

--- Api/Rest/Callwaiting.php
+++ Api/Rest/Callwaiting.php
@@ -1,18 +1,34 @@
 <?php
-namespace FreePBX\modules\Callforward\Api\Rest;
+namespace FreePBX\modules\Callwaiting\Api\Rest;
 use FreePBX\modules\Api\Rest\Base;
 class Callwaiting extends Base {
+	protected $module = 'callwaiting';
 	public function setupRoutes($app) {
+
+		/**
+		* @verb GET
+		* @return - a list of users' callwaiting settings
+		* @uri /callwaiting/users
+		*/
 		$app->get('/users', function ($request, $response, $args) {
 			\FreePBX::Modules()->loadFunctionsInc('callwaiting');
 			return $response->withJson(callwaiting_get());
 		})->add($this->checkAllReadScopeMiddleware());
 
+		/**
+		 * @verb GET
+		 * @returns - a users' callwaiting settings
+		 * @uri /callwaiting/users/:id
+		 */
 		$app->get('/users/{id}', function ($request, $response, $args) {
 			\FreePBX::Modules()->loadFunctionsInc('callwaiting');
 			return $response->withJson(callwaiting_get($args['id']));
 		})->add($this->checkAllReadScopeMiddleware());
 
+		/**
+		* @verb PUT
+		* @uri /callwaiting/users/:id
+		*/
 		$app->put('/users/{id}', function ($request, $response, $args) {
 			\FreePBX::Modules()->loadFunctionsInc('callforward');
 			$params = $request->getParsedBody();
