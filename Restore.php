863528a9b907f7d89d7ff9dcc8d027c9cdb36755
Andrew Nagy <andrew.nagy@the159.com>
2019-02-20 18:30:15 -0500

    [Module Tag script: callwaiting 15.0.4] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,25 +2,25 @@
 namespace FreePBX\modules\Callwaiting;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    /** Lets pretend we are bulk handler */
-    $final = [];
-    foreach($configs as $key => $value){
-      if(isset($value['callwaiting_enable'])) {
-        continue;
-      }
-      $final[] = ['callwaiting_enable' => $value['callwaiting_enable'], 'extension' => $key]; 
-    }
-    $this->FreePBX->Callwaiting->bulkhandlerImport('extensions',$final);
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $cw = $this->FreePBX->Callwaiting;
-    $astdb = $this->getAstDb($tmpfiledir . '/astdb');
-    if (isset($astdb['CW'])) {
-      foreach ($astdb['CW'] as $exten => $val) {
-        $cw->setStatusByExtension($exten, $val);
-      }
-    }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		/** Lets pretend we are bulk handler */
+		$final = [];
+		foreach($configs as $key => $value){
+			if(isset($value['callwaiting_enable'])) {
+				continue;
+			}
+			$final[] = ['callwaiting_enable' => $value['callwaiting_enable'], 'extension' => $key];
+		}
+		$this->FreePBX->Callwaiting->bulkhandlerImport('extensions',$final);
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$cw = $this->FreePBX->Callwaiting;
+		$astdb = $data['astdb'];
+		if (isset($astdb['CW'])) {
+			foreach ($astdb['CW'] as $exten => $val) {
+				$cw->setStatusByExtension($exten, $val);
+			}
+		}
+	}
 }
